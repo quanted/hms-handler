5ca29ef8cf2f8b9e5de614868e948b8a63f19818
douglaspatton@gmail.com <douglaspatton@gmail.com>
2021-01-12 10:34:15 -0500

    starting ecoregions plot

--- accuracy_assessment.py
+++ accuracy_assessment.py
@@ -1,8 +1,7 @@
-
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 import re
-from vb_estimators import L1Lars
 import logging
 from statsmodels.api import OLS,add_constant
 
@@ -15,27 +14,29 @@ class DataTool:
             'data_sources':['nldas'], #'gldas'
             'accuracy_metrics':['pearsons'], #'pearsons'
             'exclusions':['monthly','yearly','none','25p'], # 'nonzero','monthly',''
-            'geog_covs':['section'], #geographic covariates
-            'regressiondict':{
-                'interaction':False,
-                'pipeline':L1Lars,
-            }
+            'geog_covs':['province'], #geographic covariates
         }
         self.geog_names=['division','province','section'] #descending size order
+        self.expand_geog_names=False #True will append larger geogs names in hierarchy to smaller ones. e.g., to see which province a section is, etc.
         
     def describe_data(self):
         print('first 5 rows:', self.c_stats.head())
         print('pandas describe:',self.c_stats.describe())
     
-    def run_acc_compare(self,regressiondict=None):
-        if regressiondict is None:
-            regressiondict=self.modeldict['regressiondict']
+    def plot_acc_compare(self):
+        try:self.model
+        except: self.run_acc_compare()
+        self.eco=gpd.read_file('ecoregions/Data/eco_us.dbf')
+    
+    def run_acc_compare(self,print_summary=False):
+        #if regressiondict is None:
+        #    regressiondict=self.modeldict['regressiondict']
         if self.flat_c_stats_df is None:
             self.set_flat_c_stats_df()
         data_df=self.flat_c_stats_df
         data_df.dropna(inplace=True,axis=0)
-        y_df=data_df.loc[:,'acc']
-        X_df=data_df.drop(labels='acc',axis=1,inplace=False)
+        y_df=data_df.loc[:,'accuracy']
+        X_df=data_df.drop(labels='accuracy',axis=1,inplace=False)
         #print('y_df',y_df)
         #print('X_df',X_df)
         X_dtypes_=dict(X_df.dtypes)
@@ -47,7 +48,10 @@ class DataTool:
         self.X_float_df=X_float_df;self.y_df=y_df
         self.model=OLS(y_df,X_float_df)
         self.model_result=self.model.fit()
-        print(self.model_result.summary())
+        if print_summary:
+            print('OLS results for modeldict:')
+            print(self.modeldict)
+            print(self.model_result.summary())
         
     def floatify_df(self,df,obj_vars):
         cols=df.columns.to_list()
@@ -73,25 +77,24 @@ class DataTool:
         geog_cols=self.modeldict['geog_covs']
         
         c_stats=self.c_stats
-        c_stats=self.set_geog_names(c_stats,geog_cols)
+        if self.expand_geog_names:
+            c_stats=self.set_geog_names(c_stats,geog_cols)
         filter_names=self.modeldict_filter_filters(c_stats.columns.to_list())
         self.filter_names=filter_names
-        #filter_name_splits=[re.split('_',fil) for fil in filter_names]
         df_index=[]
-        #ydata=[]
-        data={'acc':[],'filter':[],'source':[],'metric':[],**{var:[] for var in geog_cols}}
+        data={'accuracy':[],'filter':[],'source':[],'metric':[],**{var:[] for var in geog_cols}}
         comids=c_stats.loc[:,'comid']
         comid_count=len(comids)
         for f_i,fil in enumerate(filter_names):
             name_parts=re.split('_',fil)
-            print(name_parts)
+            #print(name_parts)
             fil_name='_'.join(name_parts[1:-1]) 
             #if len(fil_name)==0:fil_name='none'
             src=name_parts[0]
             met=name_parts[-1]
-            print(src,met)
+            #print(src,met)
             #ydata.append(c_stats.loc[:,fil])
-            data['acc'].extend(c_stats.loc[:,fil])
+            data['accuracy'].extend(c_stats.loc[:,fil])
             df_index.extend([(comid,f_i)for comid in comids])
             
             for key in geog_cols:
@@ -104,34 +107,41 @@ class DataTool:
         self.flat_c_stats_df=pd.DataFrame(data,index=df_index)
         
     def set_geog_names(self,df,geog_cols):
-        for col in geog_cols[:0:-1]: #backwards but not item 0
+        for col in geog_cols[::-1]:
             g_loc=self.geog_names.index(col)
-            df.loc[:,col]=self.str_sum(df.loc[:,self.geog_names[:g_loc+1]])
+            parent_geogs=df.loc[:,self.geog_names[:g_loc+1]]
+            df.loc[:,col]=self.str_sum(parent_geogs)
+            
         return df
     
     def str_sum(self,df):
         cols=df.columns.to_list()
         ser=df.loc[:,cols.pop(-1)]
         for col in cols:
-            ser=ser+'_'+df.loc[:,col]
+            ser=ser+f'__{col}-'+df.loc[:,col]
         return ser
             
             
-    def modeldict_filter_filters(self,filter_names):
+    def modeldict_filter_filters(self,var_names_to_filter):
+        """
+        starting with a list of variable names with distinct parts 
+        separated by '_', keeps/removes based on inclusions/exclusions 
+        in self.modeldict
+        """
         exclusions=dict.fromkeys(self.modeldict['exclusions'])#dict for fast search
         model_metrics=dict.fromkeys(self.modeldict['accuracy_metrics'])
         sources=dict.fromkeys(self.modeldict['data_sources'])
         
-        filter_name_splits=[re.split('_',fil) for fil in filter_names]
-        for f_i,fil_name in enumerate(filter_name_splits):
-            if len(fil_name)==2:
-                filter_name_splits[f_i]=[fil_name[0],'none',fil_name[1]]
+        var_name_splits=[re.split('_',var) for var in var_names_to_filter]
+        for v_i,fil_name in enumerate(var_name_splits):
+            if len(var_name)==2:
+                var_name_splits[v_i]=[var_name[0],'none',var_name[1]]
                 
-        keep_idx=[i for i,name_parts in enumerate(filter_name_splits) 
+        keep_idx=[i for i,name_parts in enumerate(var_name_splits) 
                   if name_parts[-1] in model_metrics 
                   and name_parts[0] in sources 
                   and not '_'.join(name_parts[1:-1]) in exclusions]
-        return [filter_names[i] for i in keep_idx]
+        return [var_names_to_filter[i] for i in keep_idx]
     
     
         

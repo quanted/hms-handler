73740f989039682f5d155e562cb85d5d57da6373
douglaspatton@gmail.com <douglaspatton@gmail.com>
2021-01-28 08:40:28 -0500

    model specs split up

--- run_big_model.py
+++ run_big_model.py
@@ -1,12 +1,29 @@
 from cn_correct import CompareCorrect as CC
 from warnings import filterwarnings
+from traceback import format_exc
 
 if __name__=='__main__':
     filterwarnings('ignore')
-    cc=CC()
-    print('modeldict',cc.modeldict)
-    cc.runBigModel()
-    print('complete')
+    model_spec_list=[
+        {'gbr':{
+            'kwargs':{},#these pass through to sklearn's gbr
+                #'n_estimators':10000,
+                #'subsample':1,
+                #'max_depth':3
+                }},
+        {'lin-reg':{'max_poly_deg':3,'fit_intercept':False}},
+        {'lasso':{'max_poly_deg':3,'fit_intercept':False}},
+    ]
+    for model_spec in model_spec_list:
+        try:
+            cc=CC()
+            cc.modeldict['model_specs']=model_spec
+            print('modeldict',cc.modeldict)
+            cc.runBigModel()
+            print('complete')
+        except:
+            print(format_exc())
+            
     
     
 
